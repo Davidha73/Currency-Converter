85705cb174eacd3662f728701d4161840c634d7a
David Harries <davidha73@googlemail.com>
2025-08-04 20:44:19 +0100

    various updates to all pages

--- Service-worker.js
+++ Service-worker.js
@@ -1,5 +1,5 @@
 // 12345
-const CACHE_NAME = "currency-converter-v2.3"; // Increment cache version for new changes
+const CACHE_NAME = "currency-converter-v2.31"; // Increment cache version for new changes
 const urlsToCache = [
   "/", // Root path, typically your index.html
   "/index.html",
